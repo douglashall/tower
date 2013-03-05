b9508b9436b2b9bf04425ae91d566d11646b00c1
Douglas Hall <douglas_hall@hms.harvard.edu>
2013-03-05 13:04:23 -0500

    turned off stacktrace.log

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -79,6 +79,10 @@ log4j = {
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
+	
+	appenders {
+	    null name:'stacktrace'
+	}
 
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
 	       'org.codehaus.groovy.grails.web.pages',          // GSP
